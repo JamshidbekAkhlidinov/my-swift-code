f553d8d241d393ee4588183a1efd20a413e0f451
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2024-01-12 00:13:14 +0500

    learn enum and functions

--- enum/enum/Enum.swift
+++ enum/enum/Enum.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+enum WeekDay: String{
+    case monday = "Monday"
+    case tuesday
+    case wendesday
+    case thurthday
+    case friday
+    case saturday
+    case sunday
+    
+    var helloDay:String {
+        return "Salom \(rawValue)"
+    }
+}
+
+
+enum Message {
+    case success(code:Int)
+    case error(message:String)
+}
+
+
+enum Coin: Int {
+    case penny = 1
+    case nickel = 5
+    case dime = 10
+    case quarter = 25
+}
+
--- enum/enum/main.swift
+++ enum/enum/main.swift
@@ -7,5 +7,57 @@
 
 import Foundation
 
-print("Hello, World!")
+var day:WeekDay = .monday
+var uzDay = "";
+
+switch day {
+    case .monday:
+        uzDay = "Duyshanba"
+    case .tuesday:
+        uzDay = "Seyshanba"
+    case .wendesday:
+        uzDay = "Chorshanba"
+    case .thurthday:
+        uzDay = "Payshanba"
+    case .friday:
+        uzDay = "Juma"
+    case .saturday:
+        uzDay = "Shanba"
+    case .sunday:
+        uzDay = "Yakshanba"
+}
+
+print(day.rawValue, "-", uzDay)
+print(day.helloDay)
+
+var message = Message.error(message: "Salom qalesan")
+
+switch message {
+case .error(let message):
+    print(message)
+case .success(let code):
+    print(code)
+}
+
+
+var coinpurse: [Coin] = [.penny, .quarter, .nickel, .dime, .penny, .dime, .quarter]
+
+func addCoin(_ coin:Coin, to coinpurse: inout [Coin])->Int{
+    coinpurse.append(coin)
+    var sum = 0
+    coinpurse.forEach { coin0 in
+        sum += coin0.rawValue
+    }
+    return sum
+}
+
+
+var sum = addCoin(.dime, to: &coinpurse)
+print(sum)
+
+sum = addCoin(.dime, to: &coinpurse)
+print(sum)
+
+sum = addCoin(.penny, to: &coinpurse)
+print(sum)
 
