e9fb774abc8138dedf640672677670d362361940
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2024-01-14 15:34:30 +0500

    learn tabbar controller

---
+++ UI/tabbarcontroller/tabbarcontroller/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  tabbarcontroller
+//
+//  Created by ustadev.uz on 14/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+class FavouriteViewController : UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
+class EditViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
