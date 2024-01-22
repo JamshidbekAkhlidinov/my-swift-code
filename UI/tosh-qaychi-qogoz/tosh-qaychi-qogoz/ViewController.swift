de701c2a368fb060767a47e08b3d5f3dc31c310c
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2024-01-22 22:48:20 +0500

    create game

---
+++ UI/tosh-qaychi-qogoz/tosh-qaychi-qogoz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tosh-qaychi-qogoz
+//
+//  Created by ustadev.uz on 22/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
