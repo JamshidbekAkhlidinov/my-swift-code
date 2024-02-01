e7c4ecdbe15dc349050b5ef6df8f4717b26d2d54
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2024-02-01 18:00:06 +0500

    create service and cell controller

---
+++ UI/weatherapp/weatherapp/controllers/cell/WeatherListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherListTableViewCell.swift
+//  weatherapp
+//
+//  Created by ustadev.uz on 01/02/24.
+//
+
+import UIKit
+
+class WeatherListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ UI/weatherapp/weatherapp/networking/WebService.swift
@@ -0,0 +1,20 @@
+//
+//  WebService.swift
+//  weatherapp
+//
+//  Created by ustadev.uz on 01/02/24.
+//
+
+import UIKit
+
+class WebService: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
